bb286aacb4ed717b03a8e0908991a69efbe3d56e
Michael Kelley <michael@silvertreksystems.com>
2014-02-05 22:35:54 -0800

    cleaning up and going to sleep on 2-5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let (:base_title) {'Ruby on Rails Tutorial Sample App |'}
+	let (:base_title) {'Ruby on Rails Tutorial Sample App'}
 
   describe "Home page" do
 
@@ -13,7 +13,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the wrong title" do 
+      visit '/static_pages/home'
+      expect(page).not_to have_title("#{base_title} | Home")
     end
   end
 
@@ -26,7 +31,7 @@ describe "Static pages" do
 
   	it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -39,7 +44,7 @@ describe "Static pages" do
 
   	it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -52,7 +57,7 @@ describe "Static pages" do
 
   	it "should have the right title" do
   		visit '/static_pages/contact'
-  		expect(page).to have_title("#{base_title} Contact")
+  		expect(page).to have_title("#{base_title} | Contact")
   	end
   end
 end
